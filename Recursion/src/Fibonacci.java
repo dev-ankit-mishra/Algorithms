426f57533a8d79313a0c51f4815e5ab2e1feafc1
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-01-03 19:34:49 +0530

    Fibonacci Number

---
+++ Recursion/src/Fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//Fibonacci Program
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(fibo(n));
+    }
+    public static int fibo(int n){
+        if(n<2){
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+
+}
