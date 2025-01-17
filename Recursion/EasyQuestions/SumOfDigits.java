7a4610653b52314e992edabf81eef30e9b79b5af
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-01-17 22:31:17 +0530

    Sum of Digits

---
+++ Recursion/EasyQuestions/SumOfDigits.java
@@ -0,0 +1,12 @@
+//Sum of Digits
+public class SumOfDigits {
+    public static void main(String[] args) {
+        System.out.println(sumOfDigits(1234));
+    }
+    public static int sumOfDigits(int n){
+        if(n%10==n){
+            return n;
+        }
+        return (n%10) + sumOfDigits(n/10);
+    }
+}
