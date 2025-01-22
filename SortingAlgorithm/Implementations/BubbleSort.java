45c7efc4301eb333d233950b3aec143fc5d8a42e
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-01-22 21:56:35 +0530

    Bubble Sort

--- SortingAlgorithm/Implementations/BubbleSort.java
+++ SortingAlgorithm/Implementations/BubbleSort.java
@@ -13,7 +13,24 @@ public class BubbleSort {
 
     }
 
-    //Method-3(Optimized Solution)
+    //Method-1
+    public static void sort2(int[] arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+
+                }
+            }
+
+        }
+    }
+
+    //Method-2(Optimized Solution)
     public static void sort3(int[] arr){
         int n=arr.length;
         for(int i=0;i<n-1;i++){
