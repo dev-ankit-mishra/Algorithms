6e775b8090b68596bffcbabe25881ecd1c8c18f9
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-02-21 22:18:19 +0530

    Kth Missing Positive Number[LeetCode-1539]

---
+++ BinarySearch/Questions/KthMissingPositiveNumber.java
@@ -0,0 +1,18 @@
+//Problem Link-https://leetcode.com/problems/kth-missing-positive-number/
+public class KthMissingPositiveNumber {
+    public int findKthPositive(int[] arr, int k) {
+        int lo=0;
+        int hi=arr.length-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            int missed=arr[mid]-(mid+1);
+            if(missed<k){
+                lo=mid+1;
+            }else{
+                hi=mid-1;
+            }
+        }
+        return k+hi+1;
+
+    }
+}
