9d66d7a51246a5ca43a6a77dfaa86b4824644595
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-03-02 12:16:27 +0530

    SubsetsII [LeetCode-90]

---
+++ BitManipulations/Questions/SubsetsII.java
@@ -0,0 +1,25 @@
+//Problem Link-https://leetcode.com/problems/subsets-ii/?envType=problem-list-v2&envId=bit-manipulation
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        int n=nums.length;
+        int s=1<<n;
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=0;i<s;i++){
+            List<Integer> arr=new ArrayList<>();
+            for(int j=0;j<n;j++){
+                if((i&(1<<j))!=0){
+                    arr.add(nums[j]);
+                }
+            }
+            Collections.sort(arr);
+            if(!ans.contains(arr))
+                ans.add(arr);
+        }
+        return ans;
+
+    }
+}
