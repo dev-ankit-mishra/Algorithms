637765aaa5d5176b72e71cfb30fd0617c17d94d5
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-04-15 21:35:50 +0530

    Fibonacci Memoization[LeetCode-509]

---
+++ Dynamic Programming/Questions/FibonacciMemoization.java
@@ -0,0 +1,17 @@
+//Problem Link-https://leetcode.com/problems/fibonacci-number/description/
+
+public class FibonacciMemoization {
+    public int fib(int n) {
+        int dp[]=new int[n+1];
+        return fibo(n,dp);
+    }
+    public int fibo(int n,int[] dp){
+        if(n<=1){
+            return n;
+        }
+        if(dp[n]!=0) return dp[n];
+        return dp[n]=fibo(n-1,dp) + fibo(n-2,dp);
+    }
+
+
+}
